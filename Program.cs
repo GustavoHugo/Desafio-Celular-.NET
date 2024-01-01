aabfd65db1ddc6a1ebb9fa58576ea4c2855cb857
GustavoHugo <gustavo.hugo@aluno.faculdadeimpacta.com.br>
2024-01-01 16:04:08 -0300

    Desafio celular resolvido

--- Program.cs
+++ Program.cs
@@ -1,3 +1,16 @@
-﻿using DesafioPOO.Models;
+﻿using System.ComponentModel;
+using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+Console.WriteLine("Celular da Nokia");
+Smartphone nokia = new Nokia(numero: "(99) 99999-9999", modelo: "Nokia 3310", imei: "1111111111", memoria: 1);
+nokia.Ligar();
+nokia.ReceberLigacao();
+nokia.InstalarAplicativo("Jogo da Cobrinha");
+
+Console.WriteLine("\n");
+
+Console.WriteLine("Celular Iphone");
+Smartphone iphone = new Iphone(numero: "(77) 77777-7777", modelo: "iPhone", imei: "5555555555", memoria: 4);
+iphone.Ligar();
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo("Whatsapp");
